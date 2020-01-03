2784fe16c683e778cfd0f2b45e907400259758e5
cheshamchou <cheshamchou@moneydj.com>
2020-01-03 14:10:47 +0800

    first commit

---
+++ cppdemo/cppdemo.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <nlohmann/json.hpp> // 因為專案 include 有設定 "../.submodules/nlohmann/include" 才能這樣 include
+using namespace std;
+using namespace nlohmann;
+
+int main()
+{
+    auto input = R"({
+  "pi": 3.141,
+  "happy": true,
+  "name": "Niels",
+  "nothing": null,
+  "answer": {
+    "everything": 42
+  },
+  "list": [1, 0, 2],
+  "object": {
+    "currency": "USD",
+    "value": 42.99
+  }
+})";
+    // json object
+    auto jsonObj = json::parse(input);
+    // serialize
+    cout << jsonObj;
+}
